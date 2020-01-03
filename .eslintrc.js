bc84b0036868c31a10ab13749ddd0daa342b4c61
Taylor Moineau <48955070+taylormoineau@users.noreply.github.com>
2020-01-03 17:17:11 -0600

    Added tester pics, added a bunch of npm stuff and created a few small things.
    Ugh... I changed a bunch of things and added a bunch of test pictures for a template.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {jsx: true}
   },
@@ -11,16 +11,25 @@ module.exports = {
     'prettier',
     'plugin:react/recommended',
     'plugin:sonarjs/recommended',
+    "plugin:import/errors",
+    "plugin:jsx-a11y/recommended",
+    "prettier/react"
   ],
-  plugins: ['prettier', 'react', 'sonarjs'],
+  plugins: ['prettier', 'react', 'sonarjs', "jsx-a11y", "react-hooks"],
   rules: {
-    'no-console': 'off',
+    'no-console': 'warn',
     'prettier/prettier': ['warn', {singleQuote: true, bracketSpacing: false}],
     'dot-notation': 'warn',
     'quote-props': ['warn', 'as-needed'],
     'arrow-body-style': ['warn', 'as-needed'],
     'object-shorthand': 'warn',
     'sonarjs/cognitive-complexity': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    "react-hooks/rules-of-hooks": "error"
+  },
+   "settings": {
+    "react": {
+      "version": "detect"
+    }
   }
 };
