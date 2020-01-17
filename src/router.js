ee58bc4b23fcedc4e155099d897828c5d0a34bbe
Taylor Moineau <taylormoineau@gmail.com>
2020-01-16 19:26:12 -0600

    created router

---
+++ src/router.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import App from './App';
+
+export const DootRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login Page</Link>
+          </li>
+          <li>
+            <Link to="/adminpage">Admin Page</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <About />
+        </Route>
+        <Route path="/adminpage">
+          <App />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
