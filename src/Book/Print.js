b59ef59172d33b52d10e326232ea13176b28244a
Taylor Moineau <48955070+taylormoineau@users.noreply.github.com>
2020-03-18 11:27:27 -0500

    wip

--- src/Book/Print.js
+++ src/Book/Print.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { loadData } from '../utils';
-import { Link, useHistory } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import templatePageRight from './templatePageRight.png';
-import templatePageLeft from './templatePageLeft.png';
+import React, {useState, useEffect} from 'react';
+import {loadData} from '../utils';
+import {Link, useHistory} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import borderTemplate from './borderTemplate.png';
 import './print.css';
 
 export const Print = () => {
@@ -11,7 +10,7 @@ export const Print = () => {
   const [bookState, setBookState] = useState([]);
   const [printFail, setPrintFail] = useState(false);
   const [error, setError] = useState('');
-  const { bookId } = useParams();
+  const {bookId} = useParams();
 
   useEffect(() => {
     loadData('getOneBook/' + bookId, setBookState, setError);
@@ -21,17 +20,13 @@ export const Print = () => {
   return (
     <div className="printDiv">
       <h2>Print Preview of Book:</h2>
-      <h3 style={{ color: 'red' }}>{error}</h3>
-      <div className="templateDivLeft">
-        {pages.map(({ id, page_number, page_description, page_image }) => (
+      <h3 style={{color: 'red'}}>{error}</h3>
+      <div>
+        {pages.map(({id, page_number, page_description, page_image}) => (
           <div key={id}>
             {page_number < 4 && (
               <div className="printDiv">
-                <img
-                  src={templatePageLeft}
-                  alt="template"
-                  className="background"
-                />
+                <img src={borderTemplate} alt="template" className="template" />
 
                 <p className="leftPageNumber">{page_number + 1}</p>
 
@@ -40,31 +35,6 @@ export const Print = () => {
                 <img
                   src={page_image}
                   className="leftImage"
-                  alt={'Page number: ' + page_number}
-                />
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-      <div className="templateDivRight">
-        {pages.map(({ id, page_number, page_description, page_image }) => (
-          <div key={id}>
-            {page_number < 4 && (
-              <div className="printDiv">
-                <img
-                  src={templatePageRight}
-                  alt="template"
-                  className="background"
-                />
-
-                <p className="rightPageNumber">{page_number + 1}</p>
-
-                <p className="rightText">{page_description}</p>
-
-                <img
-                  src={page_image}
-                  className="rightImage"
                   alt={'Page number: ' + page_number}
                 />
               </div>
